38d24780c011c1eded11d90c01284d8d9cb12abd
Bruno Tenório <tenoriobn@gmail.com>
2024-11-23 21:02:58 -0300

    23-11-2024/feature - adicionando seção de aula demonstrativa

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,11 @@ import './accordion.js';
 import './swiper.js';
 import './player.js'
 import 'video.js/dist/video-js.css';
+
+
+import Plyr from 'plyr';
+
+const player = new Plyr('#player', {
+    controls: ['play-large', 'play', 'progress', 'volume', 'fullscreen'],
+    autoplay: false,
+});
